ad356cd6a2cb775b18cc0abb715adfcd5440e1ee
jonathan <jonathan.sahar@campus.technion.ac.il>
2016-06-02 14:12:07 +0300

    Add get_RBP_motifs_all_genes

--- feature extraction.py
+++ feature extraction.py
@@ -1,6 +1,7 @@
 from cogent.parse.consan import sequence
 from collections import Counter
 from itertools import combinations
+import re
 
 NUCLEOTIDES = "ACUG"
 def count_nucleotides(sequence):
@@ -35,7 +36,7 @@ PVALUE  = 4
 workspace = "/home/jonathan/Documents/data"
 all_RBPs = {}
 
-def get_RBP_motifs(gene_name):
+def get_RBP_motifs_from_gene(gene_name):
     motif = "a - motif"
     occurances = "b - occurances"
     positions = "c - positions"
@@ -48,6 +49,7 @@ def get_RBP_motifs(gene_name):
     f_name = workspace + "/rbp_data.tmp"
     f_name = workspace + "/rbp_motifs/prediction_example.txt"
     # get_motifs_from_rbpMap(f_name)
+
     prev_protein = ""
     protein = ""
     bNew_motif = False
@@ -69,7 +71,8 @@ def get_RBP_motifs(gene_name):
                              all_RBPs[protein] = [gene_name]
 
                         # calculate stats for previous protein
-                        dMotif_counts[protein][average_pvalue] = np.average(np.array([float(x) for x in dMotif_counts[protein][p_values]]))
+                        dMotif_counts[protein][average_pvalue] = np.average(np.array([float(x)
+                                                                            for x in dMotif_counts[protein][p_values]]))
 
                     # For all including the first protein in the file
                     protein = line[1]
@@ -91,11 +94,59 @@ def get_RBP_motifs(gene_name):
         # This is a list of lists (will hold all the lines in the file):
         #   - the outer list is created from dMotif_counts and sorted by the protein name.
         #   - the inner list is sorted by field name, and sets the order of the fields in the output file.
-        lines = [[prot] + [v for k, v in sorted(dFields.items(), key= lambda tup: tup[0])] for prot, dFields in sorted(dMotif_counts.items(), key= lambda tup: tup[0])]
-        header = ["protein_name", "protein_motif", "num_occurences", "AUG_postions", "k-mers", "p-values", "average_pvalue" ]
+
+        def key_tuple_by_first(tup):
+            return tup[0]
+
+        def sort_by_first_in_tuple(list_of_tuples):
+            return sorted(list_of_tuples, key=key_tuple_by_first)
+
+        sorted_proteins_and_counts = sort_by_first_in_tuple(dMotif_counts.items())
+
+        lines = [
+                [gene_name, prot] +
+                [value for key, value in sort_by_first_in_tuple(dFields.items())] for prot, dFields in sorted_proteins_and_counts
+                ]
+
+        header = ["gene_name", "protein_name", "protein_motif", "num_occurences", "AUG_postions", "k-mers", "p-values", "average_pvalue" ]
+
         print header
         for l in lines:
             print l
         print all_RBPs
 
-get_RBP_motifs("my_gene")
+def get_RBP_motifs_all_genes():
+    f_name = workspace + "/rbp_motifs/prediction_example_multiple_genes.txt"
+    collect_lines = False
+    lines = []
+    extrated_features = []
+    gene_name = ""
+    with open(f_name) as file:
+        for line in file:
+            line = line.split()
+            if len(line) < 1 :
+                continue
+
+            # finish a gene section
+            if re.match(r'\*+ ', line[0]):
+                collect_lines = False
+                extrated_features += get_RBP_motifs_from_gene(gene_name)
+                continue
+
+            # add line to be processed by get_RBP_motifs
+            if collect_lines:
+                lines.append(line)
+                continue
+
+            # this line is a gene name
+            if re.match(r'ENSMUST.*', line[0]):
+                gene_name = re.match(r'ENSMUST.*', line[0]).group(0)
+                continue
+
+            # start a new gene section
+            if re.match(r'=+ ', line[0]):
+                collect_lines = True
+                continue
+
+
+get_RBP_motifs_from_gene("my_gene")
